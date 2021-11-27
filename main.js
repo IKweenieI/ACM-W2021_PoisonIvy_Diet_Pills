89fa5df8d3bd91e6f610943ec99351d8aeae8623
Khushi <76661137+IKweenieI@users.noreply.github.com>
2021-11-27 21:06:18 +0530

    bro i updatwed ttis

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-for (let i = 1; i <3; i++)
+for (let i = 1; i <14; i++)
 {
-    document.getElementById(i.toString()).addEventListener("click", function (theID)
+    document.getElementById(i.toString()).addEventListener("click", function ()
     {
+        console.log(document.getElementById(i.toString()))
         document.getElementById("input-num-".concat('', i.toString())).classList.toggle("display-none");
     })
 }
